bf95a16f4ede1180f2f598b421a2eaec5beef3e7
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-20 21:02:59 +0700

    feat: simplify welcome message on home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,7 +14,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <p class="text-gray-600">Siap menguji kemampuanmu? Mulai tryout sekarang dan ukur pencapaianmu!</p>
+                    <p class="text-gray-600">Siap menguji kemampuanmu? Mulai tryout sekarang!</p>
                 </div>
                 <div class="pt-4">
                     <a href="{{ route('soal.show', 1) }}"
